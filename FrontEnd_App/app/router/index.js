99194f024d69b8225bc809dab5d64b86d42df503
snhkpark <qkr819@ajou.ac.kr>
2019-05-16 21:48:03 +0900

    communication with backend

--- FrontEnd_App/app/router/index.js
+++ FrontEnd_App/app/router/index.js
@@ -2,11 +2,13 @@ import Main_ from '../components/Main'
 import Login_ from '../components/Login'
 import Map_ from '../components/Map' 
 import Board_ from '../components/Board'
+import camera_ from '../components/camera'
 
 const router = { 
    main: Main_, 
    login: Login_,
    map: Map_,
-   board: Board_
+   board: Board_,
+   camera: camera_
 }
 export default router
