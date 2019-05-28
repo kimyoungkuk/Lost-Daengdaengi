a80744b8fde46af668bec2be02d130d11dba6600
ChanYoung Park <pgd0919@gmail.com>
2019-05-29 04:15:02 +0900

    상세보기 화면 수정

--- FrontEnd_Web/src/components/OwnerBoard.vue
+++ FrontEnd_Web/src/components/OwnerBoard.vue
@@ -95,7 +95,7 @@ export default {
         }).then(res => {
             console.log(res.data)
             this.posts = res.data
-        }).bind(this)
+        })
         alert(JSON.stringify(this.form))
         console.log(this.form)
       },
