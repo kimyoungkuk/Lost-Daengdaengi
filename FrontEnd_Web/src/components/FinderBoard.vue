e2ebe0161e3c8d0f019f9acf8b812b13e82db4e9
Soon Won Choi <wony6731@naver.com>
2019-06-03 22:13:48 +0900

    상세페이지 UI 보완

--- FrontEnd_Web/src/components/FinderBoard.vue
+++ FrontEnd_Web/src/components/FinderBoard.vue
@@ -1,59 +1,63 @@
 <template>
-<div>
+  <div>
+    <!-- 발견인 게시판 & 유기견주 게시판 선택 버튼 -->
     <div>
       <b-button-group>
-      <b-button router-link to='/finderboard' variant="outline-primary">발견인 게시판</b-button>
-      <b-button router-link to='/ownerboard' variant="outline-primary">유기견주 게시판</b-button>
-    </b-button-group>
+        <b-button router-link to='/finderboard' variant="outline-primary">발견인 게시판</b-button>
+        <b-button router-link to='/ownerboard' variant="outline-primary">유기견주 게시판</b-button>
+      </b-button-group>
     </div>
-     <div>
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-
-     <b-form-group id="input-group-2" label="검색 내용" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.input"
-          required
-          placeholder="검색 내용을 입력하세요."
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-3" label="검색 카테고리" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.category"
-          :options="categories"
-          required
-        ></b-form-select>
-      </b-form-group>
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-  </div>
+    <!-- 검색 관련 div -->
     <div>
-    <b-card-group deck deck v-for="row in formattedPosts">
-        <b-card  v-for="post in row"
-                :title="post.title"
-                :img-src="post.imageurl"
-                style="max-width: 30rem;"
-                img-top>
-            <p class="card-text">
-                <strong>ID : </strong> {{post.id}}
-            </p>
-            <p class="card-text">
-                <strong>견종 : </strong>{{post.dog_type}}
-            </p>
-            <p class="card-text">
-              <strong>찾은 날짜 : </strong>{{$moment($moment(post.find_time).format('YYYYMMDDHH'),"YYYYMMDDHH").fromNow()}}
-                <!-- <strong>찾은 날짜 : </strong>{{$moment(post.find_time).format('LLLL')}} -->
-            </p>
-            <div slot="footer">
-                <router-link :to="`/finderboard/view/${post.id}`"><b-btn variant="primary" block>상세보기</b-btn></router-link>
-            </div>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <!-- 검색 내용 입력 -->
+        <b-form-group id="input-group-2" label="검색 내용" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.input"
+            required
+            placeholder="검색 내용을 입력하세요."
+          ></b-form-input>
+        </b-form-group>
+        <!-- 검색 카테고리 -->
+        <b-form-group id="input-group-3" label="검색 카테고리" label-for="input-3">
+          <b-form-select
+            id="input-3"
+            v-model="form.category"
+            :options="categories"
+            required
+          ></b-form-select>
+        </b-form-group>
+        <!-- 제출 및 리셋 버튼 -->
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </div>
+
+    <div>
+      <b-card-group deck deck v-for="row in formattedPosts">
+        <b-card 
+          v-for="post in row"
+          :title="post.title"
+          :img-src="post.imageurl"
+          style="max-width: 30rem;"
+          img-top>
+          <p class="card-text">
+            <strong>ID : </strong> {{post.id}}
+          </p>
+          <p class="card-text">
+            <strong>견종 : </strong>{{post.dog_type}}
+          </p>
+          <p class="card-text">
+            <strong>찾은 날짜 : </strong>{{$moment($moment(post.find_time).format('YYYYMMDDHH'),"YYYYMMDDHH").fromNow()}}
+          </p>
+          <div slot="footer">
+            <router-link :to="`/finderboard/view/${post.id}`"><b-btn variant="primary" block>상세보기</b-btn></router-link>
+          </div>
         </b-card>
-    </b-card-group>
+      </b-card-group>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
--- FrontEnd_Web/src/components/HomePage.vue
+++ FrontEnd_Web/src/components/HomePage.vue
@@ -1,81 +1,45 @@
 <template>
-  <div >
-    <div >
-    <b-carousel
-      :interval="4000"
-      controls
-      indicators
-      fade
-      style="text-shadow: 1px 1px 2px #333; img-width:10px;"
-    >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="대한민국 최강 유기견 플랫폼"
-        text="지금 당장 시작하세요"
-        :img-src= "src1"
-      ></b-carousel-slide>
+  <div>
+    <div>
+      <b-carousel
+        :interval="3000"
+        controls
+        indicators
+        fade
+        style="text-shadow: 1px 1px 2px #333; img-width:10px;">
+        <!-- Text slides with image -->
+        <b-carousel-slide
+          caption="대한민국 최강 유기견 플랫폼"
+          text="지금 당장 시작하세요"
+          :img-src= "src1"
+        ></b-carousel-slide>
 
-      <!-- Slides with custom text -->
-            <b-carousel-slide
-        caption="Lost DaengDaengi"
-        text="여러분의 반려견을 지켜주세요"
-        :img-src= "src2"
-      ></b-carousel-slide>
+        <!-- Slides with custom text -->
+        <b-carousel-slide
+          caption="반려견을 위한 시작"
+          text="여러분의 반려견을 지켜주세요"
+          :img-src= "src2"
+        ></b-carousel-slide>
 
-                  <b-carousel-slide
-        caption="Lost DaengDaengi"
-        text="자동화된 시스템을 경험하세요"
-        :img-src= "src3"
-  
-      ></b-carousel-slide>
-
-      <!-- <b-carousel-slide img-src="https://cdn.pixabay.com/photo/2016/01/19/17/41/friends-1149841_960_720.jpg">
-        <h1>여러분의 반려견을 지켜주세요</h1>
-      </b-carousel-slide> -->
-
-      <!-- Slides with image only -->
-      <!-- <b-carousel-slide img-src="https://cdn.pixabay.com/photo/2016/11/29/05/09/child-1867463_960_720.jpg">
-        <h1>자동화된 찾기 시스템을 경험하세요</h1>
-      </b-carousel-slide> -->
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <!-- <b-carousel-slide>
-        <img
-          slot="img"
-          class="d-block img-fluid w-100"
-          width="1024"
-          height="480"
-          src="https://picsum.photos/1024/480/?image=55"
-          alt="image slot"
-        >
-      </b-carousel-slide> -->
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <!-- <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide> -->
-    </b-carousel>
+        <b-carousel-slide
+          caption="Lost DaengDaengi"
+          text="자동화된 시스템을 경험하세요"
+          :img-src= "src3"
+        ></b-carousel-slide>
+      </b-carousel>
     </div>
-    <!-- <p class="mt-4">
-      Slide #: {{ slide }}<br>
-      Sliding: {{ sliding }}
-    </p> -->
-      <div>
-    <b-card
-      class="flex"
-      title="2019 Capstone Design"
-    >
-    <b-card-text>
-        <strong>Team Bohemian Capsody <br/> AJOU Univ.<br/></strong>&copy; 2019 김영국 | 박신혁 | 박찬영 | 최순원 
-      </b-card-text>
-    </b-card>
-      </div>
+
+    <div>
+      <b-card
+        class="flex"
+        title="2019 Capstone Design">
+        <b-card-text>
+          <strong>Team Bohemian Capsody <br/> AJOU Univ.<br/></strong>&copy; 2019 김영국 | 박신혁 | 박찬영 | 최순원 
+        </b-card-text>
+      </b-card>
+    </div>
+    
   </div>
-  
 </template>
 
 <script>
