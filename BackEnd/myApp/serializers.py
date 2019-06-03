0fcd73f23811022973a057db1129b838492c7548
kyk <kyk1047715@gmail.com>
2019-06-04 01:30:25 +0900

    se

--- BackEnd/myApp/serializers.py
+++ BackEnd/myApp/serializers.py
@@ -43,23 +43,27 @@ class ReportSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class NearSerializer(serializers.ModelSerializer):
-    class Meta:
-        # model = Owner_post
-        fields = ('lat', 'lng',)
+    # class Meta:
+    #     model = Owner_post
+    #     fields = ('lat', 'lng',)
+    fields = ('lat', 'lng',)
 
 class GetImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        # model = Finder_post
-        fields = ('image',)
+    # class Meta:
+    #     model = Finder_post
+    #     fields = ('image',)
+    fields = ('image',)
 
 class FilteringSerializer(serializers.ModelSerializer):
-    class Meta:
-        # model = Finder_post
-        fields = ('category','input')
+    # class Meta:
+    #     model = Finder_post
+    #     fields = ('category','input')
+    fields = ('category','input')
 
 class KeySerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('key',)
+    # class Meta:
+    #     fields = ('key',)
+    fields = ('key',)
 
 #class ReportSerializer(serializers.ModelSerializer):
 #    class Meta:
