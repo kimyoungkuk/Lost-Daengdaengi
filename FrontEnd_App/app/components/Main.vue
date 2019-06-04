0ccc7c1b1e1f9e7eadce19dce2a916699f7c0307
ChanYoung Park <pgd0919@gmail.com>
2019-06-05 02:16:53 +0900

    메인 및 로그인 UI 리빙 코랄로 변경

--- FrontEnd_App/app/components/Main.vue
+++ FrontEnd_App/app/components/Main.vue
@@ -1,29 +1,29 @@
 <template>
     <Page>
-        <ActionBar title="Lost DaengDaengi Main">
+        <ActionBar class="font-weight-bold" title="Lost Daengdaengi">
           <NavigationButton icon = "~/assets/images/baseline_menu_black_18dp.png"/>
         </ActionBar>
                 <ScrollView>
           <StackLayout>     
             <WebView scaleY="1" row="0" @loadFinished="completeLoading" height="300" loaded="onWebViewLoaded" id="myWebView" :src="this.$store.state.API_WEBVIEW_URL"/>
-            <Progress color="#4ba5fa" v-show="loadingComplete" :value="loadingValue" row ="0"/>
-            <Button class="fab btn btn-active" :text="'\uf1a3' + ' 시작하기'" height="50" width="175" @tap="$goto('login')" />
+            <Progress color="#FA7268" v-show="loadingComplete" :value="loadingValue" row ="0"/>
+            <Button class="fab mybtn btn-active" :text="'\uf1a3' + ' 시작하기'" height="50" width="175" @tap="$goto('login')" />
                       <FlexboxLayout flexDirection="column" backgroundColor="#3c495e">
                         <Label text="" height="100" backgroundColor="#FFFFFF"/>
   
   <Label class="fa" height="40" backgroundColor="#FFFFFF" textWrap="true" style="text-align:center">
     <FormattedString>
-      <Span class="fa" text="2019 Capstone Design" fontWeight="bold" fontStyle="italic" style="font-size: 24px;"/>
+      <Span class="font-italic font-weight-bold" text="2019 Capstone Design" style="font-size: 24px; color: #FA7268; "/>
     </FormattedString>
   </Label>
     <Label height="30" backgroundColor="#FFFFFF" textWrap="true" style="text-align:center">
     <FormattedString>
-      <Span text="Team - Bohemian capsody" fontWeight="bold" style="font-size: 18px;"/>
+      <Span text="Team - Bohemian capsody" fontWeight="bold" style="font-size: 18px; color: #FA7268;"/>
     </FormattedString>
     </Label>
-      <Label height="30" backgroundColor="#FFFFFF" textWrap="true" style="text-align:center">
+      <Label height="30" backgroundColor="#FFFFFF" textWrap="true" style="text-align:center ">
     <FormattedString>
-      <Span text="김영국 | 박신혁 | 박찬영 | 최순원" style="font-size: 16px;"/>
+      <Span text="김영국 | 박신혁 | 박찬영 | 최순원" style="font-size: 16px; background-color:#FA7268; color: #FFFFFF;"/>
     </FormattedString>
   </Label>
   </FlexboxLayout>
@@ -35,7 +35,6 @@
 </template>
 
 <script >
-
   export default {
     data() {
       return {
@@ -66,14 +65,19 @@
 </script>
 
 <style scoped>
-@import '~nativescript-theme-core/css/core.light.css';
+@import '~nativescript-theme-core/css/ruby.css';
 	.page {
 		align-items: center;
 		flex-direction: column;
   }
     ActionBar {
-        background-color: #4ba5fa;
+        background-color: #FA7268;
         color: #ffffff;
+    }
+
+    .mybtn{
+      color: #ffffff;
+      background-color: #FA7268;
     }
 
     .message {
