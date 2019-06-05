6722e600879dc48f633e20e1afe42f28ab1622eb
kyk <kyk1047715@gmail.com>
2019-06-05 21:23:31 +0900

    fin

--- BackEnd/myApp/views.py
+++ BackEnd/myApp/views.py
@@ -521,5 +521,5 @@ def finish_post_list(request):
     finish_finder_posts = Finder_post.objects.filter(is_finished=1).values('title','id','dog_type','lost_time','imageurl','view_count','lat','lng')
     serializer = Finder_postSerializer(finish_finder_posts, many = True)
     finish_owner_posts = Owner_post.objects.filter(is_finished=1).values('title','id','dog_type','lost_time','imageurl','view_count','lat','lng')
-    serializer = Owner_postSerializer(finish_owner_posts many = True)
+    serializer = Owner_postSerializer(finish_owner_posts, many = True)
     return Response(finish_finder_posts.data+finish_owner_posts.data)
