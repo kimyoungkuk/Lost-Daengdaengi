b252226c1dc6870103111a75b25ca43fec2745fb
kyk <kyk1047715@gmail.com>
2019-06-07 04:49:40 +0900

    com

--- FrontEnd_Web/src/components/FinderBoard.vue
+++ FrontEnd_Web/src/components/FinderBoard.vue
@@ -37,8 +37,8 @@
           ></b-form-select>
         </b-form-group>
         <!-- 제출 및 리셋 버튼 -->
-        <b-button type="submit" variant="primary">Submit</b-button>
-        <b-button type="reset" variant="danger">Reset</b-button>
+        <b-button type="submit" variant="primary">Submit{{this.$store.state.user_key}}</b-button>
+        <b-button type="reset" variant="danger">Reset{{this.$store.state.user_nickname}}</b-button>
       </b-form>
     </div>
 
