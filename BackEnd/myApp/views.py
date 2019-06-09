b6bf56d1a833372db6d681b29fc48724d6b49b7c
kyk <kyk1047715@gmail.com>
2019-06-09 23:25:33 +0900

    poster

--- BackEnd/myApp/views.py
+++ BackEnd/myApp/views.py
@@ -213,13 +213,15 @@ def owner_post_create(request):
         font = ImageFont.truetype("media/poster/NanumGothicExtraBold.ttf",30)
         draw.text((15,435),"연락처 : "+str(post.phone_num),(255,255,255),font=font)
         font = ImageFont.truetype("media/poster/NanumGothicExtraBold.ttf",18)
-        draw.text((15,470),"견종 : "+str(post.dog_type)+"    이름 : "+str(post.dog_name),(255,255,255),font=font)
+        draw.text((15,470),"견종 : "+str(post.dog_type),(255,255,255),font=font)
+        draw.text((220,470),"이름 : "+str(post.dog_name),(255,255,255),font=font)
         if post.dog_sex==1:
             dog_sex_temp="수컷"
         else:
             dog_sex_temp="암컷"
         
-        draw.text((15,490),"성별 : "+str(dog_sex_temp)+"    나이 : "+str(post.dog_age)+"살",(255,255,255),font=font)
+        draw.text((15,490),"성별 : "+str(dog_sex_temp),(255,255,255),font=font)
+        draw.text((220,490),"나이 : "+str(post.dog_age)+"살",(255,255,255),font=font)
         
         # lost_time_temp=datetime.datetime.strptime(post.lost_time,"%Y-%m-%d").date()
         draw.text((15,510),"실종시간 : "+str(post.lost_time[:10]),(255,255,255),font=font)
