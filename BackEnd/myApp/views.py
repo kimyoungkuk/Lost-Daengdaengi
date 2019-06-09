162c9a0c8b42f60997d2ed41781b1e7f030f525b
kyk <kyk1047715@gmail.com>
2019-06-09 18:50:30 +0900

    fin

--- BackEnd/myApp/views.py
+++ BackEnd/myApp/views.py
@@ -553,11 +553,12 @@ def finish_post_list(request):
 
 @api_view(['GET'])
 def finish_post_list_portal(request):
-    finish_finder_posts = Finder_post.objects.order_by('-id').filter(is_finished=1).values('title','id','posted_time','dog_type','find_time','imageurl','view_count','lat','lng')
+    finish_finder_posts = Finder_post.objects.order_by('-id').filter(is_finished=1)
     finder_serializer = Finder_postSerializer(finish_finder_posts, many = True)[:2]
-    finish_owner_posts = Owner_post.objects.order_by('-id').filter(is_finished=1).values('title','id','posted_time','dog_type','lost_time','imageurl','view_count','lat','lng')
+    finish_owner_posts = Owner_post.objects.order_by('-id').filter(is_finished=1)
     owner_serializer = Owner_postSerializer(finish_owner_posts, many = True)[:2]
-    return Response({'owner':finish_owner_posts, 'finder':finish_finder_posts})
+    # return Response({'owner':finish_owner_posts, 'finder':finish_finder_posts})
+    return Response(owner_serializer.data+finder_serializer.data)
 
 
 @api_view(['POST'])
