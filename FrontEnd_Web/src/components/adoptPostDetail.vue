11a10cfc0bc7f7da34eb406455651423476118c5
kyk <kyk1047715@gmail.com>
2019-06-13 00:56:30 +0900

    is

--- FrontEnd_Web/src/components/adoptPostDetail.vue
+++ FrontEnd_Web/src/components/adoptPostDetail.vue
@@ -194,6 +194,7 @@ export default {
       name: '',
       nameState: null,
       submittedNames: [],
+      is:"g",
         
       form: {
         _id: this.$route.params.id,
@@ -226,6 +227,13 @@ export default {
     };
   },
   created() {
+    if(urlParams.get('is')=='a'){
+      this.is = 'a'
+      this.$store.state.user_nickname='adopt'
+      this.$store.state.user_key='adopt_admin'
+      console.log("ZCX")
+      console.log(this.key)
+    }
     console.log("TTT")
     let urlParams = new URLSearchParams(window.location.search);
     console.log(urlParams.get('key'))
@@ -401,7 +409,7 @@ export default {
     hideDeleteSuccessConfirmModal() {
       this.$refs['delete-success-confirm-modal'].hide()
       
-      this.$router.push("/adopt/post/list");
+      this.$router.push("/adopt/post/list?is=a");
     },
     showDeleteFailConfirmModal() {
       this.$refs['delete-fail-confirm-modal'].show()
